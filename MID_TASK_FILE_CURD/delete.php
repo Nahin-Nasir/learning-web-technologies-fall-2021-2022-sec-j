773a67a77eda0fde9c4e143f61cbe743dacf60c5
Nahin Nasir <nahinnasir21@gmai.com>
2021-10-14 00:55:30 +0600

    added delete

--- MID_TASK_FILE_CURD/delete.php
+++ MID_TASK_FILE_CURD/delete.php
@@ -1,5 +1,32 @@
-<?php 
+<?php
+$id = $_GET['id'];
+if (rename('user.txt', 'newuser.txt') != 1) 
+{
+	echo 'Error ';
+	return;
+}
+$mynewuserfile = fopen('newuser.txt', 'r');
+$myfile = fopen('user.txt', 'w');
+$counter = 0;
 
-	echo $_GET['id'];
+while (!feof($mynewuserfile)) {
+	$data = fgets($mynewuserfile);
+	if ($data != "") 
 
+	{
+		$user = explode('|', $data);
+
+		if (++$counter != $id) {
+			$userData = $user[0] . '|' . $user[1] . '|' . $user[2];
+			fwrite($myfile, $userData);
+		}
+	}
+}
+
+
+fclose($myfile);
+fclose($mynewuserfile);
+$mynewuserfile = fopen('newuser.txt','w');
+fwrite($mynewuserfile,"");
+fclose($mynewuserfile);
 ?>
