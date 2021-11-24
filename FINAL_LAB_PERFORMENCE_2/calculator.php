36ae7a2ef46c17b33801a3a45ea8b7466a9c6601
Nahin Nasir <nahinnasir21@gmai.com>
2021-11-24 15:47:33 +0600

    2nd commit

---
+++ FINAL_LAB_PERFORMENCE_2/calculator.php
@@ -0,0 +1,36 @@
+<?php
+ $i1 = $_POST['name'];
+ $i2 = $_POST['id'];
+ $op = $_POST['op'];
+ switch($op)
+ {
+ case "+":
+ $output = $i1 + $i2;
+ echo $output;
+ break;
+ case "-":
+ $output = $i1 - $i2;
+ echo $output;
+ break;
+ case '*':
+ $output = $i1 * $i2;
+ echo $output;
+ break;
+ case '/':
+ $output = $i1 / $i2;
+ echo $output;
+ break;
+ case '%':
+ $output = $i1 % $i2;
+ echo $output;
+ break;
+ case '++':
+ echo $i1 ++;
+ break;
+ case '--':
+ echo --$i1;
+ break;
+ default:
+ echo $output = "Not Found...Please Enter only Integers or Decimals...";
+ }
+?>
